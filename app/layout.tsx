2cd4a98823f48a727d4167e05cb91b0f0afa5d6a
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-07-24 23:56:47 +0530

    added hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
           storageKey="portfolio"
         >
           {children}
-          <ParticlesBg className="fixed -z-10 top-0 bottom-0 left-0 right-0 h-full w-full" />
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from "@/components/component/Mode";
 import Image from "next/image";
 import IconCloud from "@/components/magicui/icon-cloud";
+import { Github, Instagram, Twitter } from "lucide-react";
 export default function Home() {
   const slugs = [
     "typescript",
@@ -29,15 +30,51 @@ export default function Home() {
     "numpy",
   ];
   return (
-    <main>
-      <header></header>
-      <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
-        <IconCloud iconSlugs={slugs} />
-      </div>
-      <ModeToggle />
-      <div className="h-screen w-full"></div>
-      <div className="h-screen w-full"></div>
-      <div className="h-screen w-full"></div>
-    </main>
+    <div className="px-4 md:px-8 lg:px-12 ">
+      <header className="py-4 flex">
+        <span className="text-xl font-semibold">SOMANSHU BHARDWAJ</span>
+        <div className="flex-1 flex justify-end items-center gap-4">
+          <a href="">
+            <Github />
+          </a>
+          <a href="">
+            <Instagram />
+          </a>
+          <a href="">
+            <Twitter />
+          </a>
+          <ModeToggle />
+        </div>
+      </header>
+      <main>
+        <div className="h-[80vh] w-full flex items-center hero">
+          <div className="flex flex-col gap-4">
+            <span className="text-xl font-semibold text-[rebeccapurple] tracking-widest">
+              SOMANSHU BHARDWAJ
+            </span>
+            <span className="text-8xl font-bold">Web Developer</span>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row justify-around ">
+          <div className="relative flex flex-col h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border  px-20 pb-20 pt-8 ">
+            <span className="text-2xl font-bold text-center">Skills</span>
+            <IconCloud iconSlugs={slugs} />
+          </div>
+          <div className="flex flex-col justify-end ">
+            <span className="text-2xl fond-bold">GitHub Stats</span>
+            <div>
+              <img src="https://github-readme-stats-eight-phi-43.vercel.app/api?username=somanshubhardwaj&show_icons=true&theme=transparent&show=prs_merged&hide_border=true" />
+              <img
+                src="https://github-readme-stats-eight-phi-43.vercel.app/api/top-langs/?username=somanshubhardwaj&theme=transparent&hide=html,jupyter%20notebook,vue,svelte,go&layout=donut&size_weight=0.5&count_weight=0.5&hide_border=true"
+                className="w-full"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="h-screen w-full"></div>
+        <div className="h-screen w-full"></div>
+        <div className="h-screen w-full"></div>
+      </main>
+    </div>
   );
 }
