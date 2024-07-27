f86ba7d74e58a97d518ae2280f41e0a3069a0820
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-07-28 00:53:14 +0530

    added project section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
 import { benefits } from "@/constants";
-
+import Image from "next/image";
+import Arrow from "@/app/assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "@/app/assets/svg/ClipPath";
 const Projects = () => {
   return (
     <Section id={"#feature"}>
@@ -12,11 +15,50 @@ const Projects = () => {
           title={"lorem Ipsum loremm 90 "}
         />
         <div className="flex flex-wrap gap-10 mb-10">
-          {benefits.map((benefit, index) => (
-            <div key={index}>
-              <div>
-                <h5>{benefit.title}</h5>
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h5 mb-5">{item.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <Image
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
+                  <Arrow />
+                </div>
               </div>
+
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <Image
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+              <ClipPath />
             </div>
           ))}
         </div>
