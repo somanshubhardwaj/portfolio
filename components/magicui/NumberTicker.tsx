d84a78d61fc789639dd4534d0e9b389682f97baf
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-08-06 11:24:01 +0530

    fixes

--- components/magicui/NumberTicker.tsx
+++ components/magicui/NumberTicker.tsx
@@ -36,7 +36,7 @@ export default function NumberTicker({
       springValue.on("change", (latest) => {
         if (ref.current) {
           ref.current.textContent = Intl.NumberFormat("en-US").format(
-            latest.toFixed(0)
+            (latest as any).toFixed(0)
           );
         }
       }),
