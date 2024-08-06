3309c6932a126c1e5e18557e36e48b0fb3f410e4
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-08-06 09:48:27 +0530

    added increasing animation

--- components/CompanyLogo.tsx
+++ components/CompanyLogo.tsx
@@ -1,29 +1,35 @@
 import { companyLogos } from "@/constants";
 import Image from "next/image";
 import React from "react";
+import NumberTicker from "./magicui/NumberTicker";
 
 const CompanyLogo = ({ className }: { className: string }) => {
   const stats = [
     {
       title: "Contributions",
-      value: "750+",
+      value: "750",
     },
     {
       title: "Commits(2024)",
-      value: "550+",
+      value: "550",
     },
     {
       title: "Stars",
-      value: "40+",
+      value: "40",
     },
   ];
   return (
     <div className={className}>
       <ul className="md:flex hidden">
         {stats.map((stat, index) => (
-          <li className="flex-1 flex flex-col justify-center items-center">
+          <li
+            className="flex-1 flex flex-col justify-center items-center"
+            key={index}
+          >
             <span className="text-n-3 font-extralight">{stat.title}</span>
-            <span className="text-3xl font-semibold">{stat.value}</span>
+            <span className="text-3xl font-semibold">
+              <NumberTicker value={parseInt(stat.value)} /> +
+            </span>
           </li>
         ))}
       </ul>
